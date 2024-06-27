32740023ac4952f9b2301abb6dd3d9d2d2fa8af0
Stephen Royle <s.j.royle@warwick.ac.uk>
2024-06-27 13:07:05 +0100

    linting

--- plots/ProteomicsComparison/Script/DataAnalysis.R
+++ plots/ProteomicsComparison/Script/DataAnalysis.R
@@ -8,7 +8,8 @@ INV <- read.delim("Output/Data/combined_inv_proteome_with_intensities.txt")
 INV <- INV[, c("so_SHORTNAME", "so_ratioWave")]
 names(INV) <- c("Gene.names", "INV")
 
-# Read in cleaned data files. Should have a col of gene names, and LFQ values (or other quantitative measure)
+# Read in cleaned data files.
+# Should have a col of gene names and LFQ values (or other quantitative measure)
 SLMV <- read.csv("Output/Data/SLMV.txt")
 ATG <- read.csv("Output/Data/ATG.txt")
 
@@ -26,90 +27,121 @@ IA <- IA[complete.cases(IA), ]
 AS <- AS[complete.cases(AS), ]
 ISA <- ISA[complete.cases(ISA), ]
 
-# change 0 to random value with mean of 100 sd of 
+# change 0 to random value with mean of 100 sd of
 ISA[ISA == 0] <- rnorm(n = length(ISA[ISA == 0]), mean = 100, sd = 10)
 
 # correlation calculation
 cor.test(x = log10(IA$INV), y = IA$ATG, method = "pearson")
 # Pearson's product-moment correlation
-# 
+#
 # data:  log10(IA$INV) and IA$ATG
 # t = 2.5867, df = 141, p-value = 0.0107
 # alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
 #  0.05046165 0.36426871
 # sample estimates:
-#       cor 
-# 0.2128472 
+#       cor
+# 0.2128472
 
-INVvsSLMV_Plot <- ggplot(IS, aes(x=INV, y=SLMV, label=Gene.names))+
+INVvsSLMV_Plot <- ggplot(IS, aes(x = INV, y = SLMV, label = Gene.names)) +
   geom_point(size = 0.5) +
   geom_text_repel(size = 1.5, max.overlaps = 10, segment.color = "#7f7f7f7f") +
-  scale_x_continuous(name="INV", trans="log2") +
-  scale_y_continuous(name="SLMV", trans="log10") +
+  scale_x_continuous(name = "INV", trans = "log2") +
+  scale_y_continuous(name = "SLMV", trans = "log10") +
   theme_classic(9)
 
-ggsave("Output/Plots/INVvsSLMV_Plot.pdf", INVvsSLMV_Plot, width = 8, height = 8,bg = "white", units = "cm")
+ggsave("Output/Plots/INVvsSLMV_Plot.pdf", INVvsSLMV_Plot,
+  width = 8, height = 8,
+  bg = "white", units = "cm"
+)
 
-INVvsATG_Plot <- ggplot(IA, aes(x=INV, y=ATG, label=Gene.names)) +
+INVvsATG_Plot <- ggplot(IA, aes(x = INV, y = ATG, label = Gene.names)) +
   geom_point(size = 0.5) +
   geom_text_repel(size = 1.5, max.overlaps = 10, segment.color = "#7f7f7f7f") +
-  scale_x_continuous(name="INV", trans = "log2") +
-  scale_y_continuous(name="ATG9A", trans="log10", limits = c(1e+6,1e+10)) +
+  scale_x_continuous(name = "INV", trans = "log2") +
+  scale_y_continuous(name = "ATG9A", trans = "log10", limits = c(1e+6, 1e+10)) +
   theme_classic(9)
 
-ggsave("Output/Plots/INVvsATG_Plot.pdf", INVvsATG_Plot, width = 8, height = 8, bg = "white", units = "cm")
+ggsave("Output/Plots/INVvsATG_Plot.pdf", INVvsATG_Plot,
+  width = 8, height = 8,
+  bg = "white", units = "cm"
+)
 
-ATGvsSLMV_Plot <- ggplot(AS, aes(x=ATG, y=SLMV, label=Gene.names)) +
+ATGvsSLMV_Plot <- ggplot(AS, aes(x = ATG, y = SLMV, label = Gene.names)) +
   geom_point(size = 0.5) +
   geom_text_repel(size = 1.5, max.overlaps = 10, segment.color = "#7f7f7f7f") +
-  scale_x_continuous(name="ATG9A", trans="log10", limits = c(1e+6,1e+10)) +
-  scale_y_continuous(name="SLMV") +
+  scale_x_continuous(name = "ATG9A", trans = "log10", limits = c(1e+6, 1e+10)) +
+  scale_y_continuous(name = "SLMV") +
   theme_classic(9)
 
-ggsave("Output/Plots/ATGvsSLMV_Plot.pdf", ATGvsSLMV_Plot, width = 8, height = 8, bg = "white", units = "cm")
+ggsave("Output/Plots/ATGvsSLMV_Plot.pdf", ATGvsSLMV_Plot,
+  width = 8, height = 8,
+  bg = "white", units = "cm"
+)
 
 ## Euler plot
 
 make_euler_2 <- function(A, B, AB, names = c("A", "B")) {
-  set1 <- c(A = A - AB,
-            B = B - AB,
-            "A&B" = AB)
+  set1 <- c(
+    A = A - AB,
+    B = B - AB,
+    "A&B" = AB
+  )
   # change names
   name <- paste(names, collapse = "&")
   names(set1) <- c(names, name)
   fit1 <- euler(set1, shape = "ellipse")
   p <- plot(fit1, quantities = TRUE)
   name <- paste(names, collapse = "_vs_")
-  ggsave(paste0("Output/Plots/",name,".pdf"), p, width = 3.5, height = 3.5, units = "cm")
+  ggsave(paste0("Output/Plots/", name, ".pdf"), p,
+    width = 3.5, height = 3.5, units = "cm"
+  )
 }
 
-make_euler_2(A = nrow(INV), B = length(unique(SLMV$SLMV_uid)), AB = length(unique(IS$SLMV_uid)), names = c("INV", "SLMV"))
-make_euler_2(A = nrow(INV), B = length(unique(ATG$ATG_uid)), AB = length(unique(IA$ATG_uid)), names = c("INV", "ATG9"))
+make_euler_2(
+  A = nrow(INV),
+  B = length(unique(SLMV$SLMV_uid)),
+  AB = length(unique(IS$SLMV_uid)),
+  names = c("INV", "SLMV")
+)
+make_euler_2(
+  A = nrow(INV),
+  B = length(unique(ATG$ATG_uid)),
+  AB = length(unique(IA$ATG_uid)),
+  names = c("INV", "ATG9")
+)
 
 make_euler_3 <- function(A, B, C, AB, BC, AC, ABC, names = c("A", "B", "C")) {
-  set1 <- c(A = A - AB - AC + ABC,
-            B = B - AB - BC + ABC,
-            C = C - BC - AC + ABC,
-            "A&B" = AB - ABC,
-            "A&C" = AC - ABC,
-            "B&C" = BC - ABC,
-            "A&B&C" = ABC)
+  set1 <- c(
+    A = A - AB - AC + ABC,
+    B = B - AB - BC + ABC,
+    C = C - BC - AC + ABC,
+    "A&B" = AB - ABC,
+    "A&C" = AC - ABC,
+    "B&C" = BC - ABC,
+    "A&B&C" = ABC
+  )
   # change names
   name <- paste(names, collapse = "&")
   names(set1[1:3]) <- names
   fit1 <- euler(set1, shape = "ellipse")
   p <- plot(fit1, quantities = TRUE)
   name <- paste(names, collapse = "_vs_")
-  ggsave(paste0("Output/Plots/",name,".pdf"), p, width = 4, height = 4, units = "cm")
+  ggsave(paste0("Output/Plots/", name, ".pdf"), p,
+    width = 4, height = 4, units = "cm"
+  )
 }
 
-make_euler_3(A = nrow(INV),
-             B = length(unique(SLMV$SLMV_uid)),
-             C = length(unique(ATG$ATG_uid)),
-             AB = length(unique(IS$SLMV_uid)),
-             BC = length(unique(AS$SLMV_uid)),
-             AC = length(unique(IA$ATG_uid)),
-             ABC = min(length(unique(ISA$ATG_uid)), length(unique(ISA$SLMV_uid))),
-             names = c("INV", "SLMV", "ATG"))
-
+make_euler_3(
+  A = nrow(INV),
+  B = length(unique(SLMV$SLMV_uid)),
+  C = length(unique(ATG$ATG_uid)),
+  AB = length(unique(IS$SLMV_uid)),
+  BC = length(unique(AS$SLMV_uid)),
+  AC = length(unique(IA$ATG_uid)),
+  ABC = min(
+    length(unique(ISA$ATG_uid)),
+    length(unique(ISA$SLMV_uid))
+  ),
+  names = c("INV", "SLMV", "ATG")
+)
--- plots/ProteomicsComparison/Script/DataCleaning.R
+++ plots/ProteomicsComparison/Script/DataCleaning.R
@@ -3,27 +3,31 @@ library(ggplot2)
 library(dplyr)
 
 ## Functions ----
-# this function will separate values with semi colons and give them new rows, then sum duplicate entries
+
+# this function will separate values with semi colons and give them new rows,
+# then sum duplicate entries
 df_to_single_protein <- function(df, Genelist) {
-  df$uid <- as.character(1:nrow(df))
+  df$uid <- as.character(seq_len(nrow(df)))
   build <- data.frame()
-  for(i in 1:length(df[,Genelist])) {
+  for (i in seq_along(df[, Genelist])) {
     brick <- data.frame()
-    gene <- unlist(strsplit(df[i,Genelist], ";"))
-    for(j in 1:length(gene)){
+    gene <- unlist(strsplit(df[i, Genelist], ";"))
+    for (j in seq_along(gene)) {
       brick <- rbind(brick, df[i, ])
       brick[j, Genelist] <- gene[j]
-    } 
-    build <- rbind(build,brick)
+    }
+    build <- rbind(build, brick)
   }
-  
+
   separated_with_uid <- build[, c(Genelist, "uid")]
   # remove rows with duplicate Genelist values (keep first)
-  separated_with_uid <- separated_with_uid[!duplicated(separated_with_uid[,Genelist]), ]
+  separated_with_uid <- separated_with_uid[!duplicated(
+    separated_with_uid[, Genelist]
+  ), ]
   # sum duplicate entries in build
   build <- ddply(build, Genelist, numcolwise(sum))
   build <- merge(build, separated_with_uid, by = Genelist, all.x = TRUE)
-  
+
   return(build)
 }
 
@@ -44,9 +48,15 @@ names(SLMV)[names(SLMV) == "Gene.Names"] <- "Gene.names"
 SLMV$Gene.names <- gsub(" ", ";", SLMV$Gene.names)
 
 # ATG9 dataset cleaning. Gets raw LFQ values from log10
-ATG$LFQ <- sapply(ATG$Intensity_ES_vs_FM_ATG9A.Immunoisolation_intensity_filtered, function(df){ 10^df})
+ATG$LFQ <- sapply(
+  ATG$Intensity_ES_vs_FM_ATG9A.Immunoisolation_intensity_filtered,
+  function(df) {
+    10^df
+  }
+)
 names(ATG)[names(ATG) == "Gene_names"] <- "Gene.names"
-ATG$FC10 <- ATG$Intensity_ES_vs_FM_ATG9A.Immunoisolation_intensity_filtered - ATG$Intensity_Mix_CTRL.IgM.Immunoisolation
+ATG$FC10 <- ATG$Intensity_ES_vs_FM_ATG9A.Immunoisolation_intensity_filtered -
+  ATG$Intensity_Mix_CTRL.IgM.Immunoisolation
 # if Intensity_Mix_CTRL.IgM.Immunoisolation is 0 then FC10 is 1
 ATG$FC10[ATG$Intensity_Mix_CTRL.IgM.Immunoisolation == 0] <- 1
 # drop rows with NA
@@ -59,22 +69,21 @@ ATG <- ATG[ATG$logFC_ES_vs_FM_ATG9A.Immunoisolation_intensity_filtered > -3, ]
 ATG <- ATG[ATG$FC10 > 0.6, ]
 
 # before simplifying ATG, we will save the cleaned data
-ATG_cleaned <- df_to_single_protein(ATG,"Gene.names")
+ATG_cleaned <- df_to_single_protein(ATG, "Gene.names")
 write.csv(ATG_cleaned, "Output/Data/ATG_cleaned.txt", row.names = FALSE)
 
 # simplify the datasets
 names(INV) <- c("Gene.names", "INV")
 names(SLMV) <- c("Gene.names", "SLMV")
-ATG <- ATG %>% 
+ATG <- ATG %>%
   select(Gene.names, LFQ)
 names(ATG) <- c("Gene.names", "ATG")
 
-# Separates values with semi colons and gives them new rows, then sums duplicate entries
-# INV <- df_to_single_protein(INV,"Gene.names") # not needed for INV
-SLMV <- df_to_single_protein(SLMV,"Gene.names")
-ATG <- df_to_single_protein(ATG,"Gene.names")
+# Separates values with semicolons and gives them new rows,
+# then sums duplicate entries
+SLMV <- df_to_single_protein(SLMV, "Gene.names")
+ATG <- df_to_single_protein(ATG, "Gene.names")
 
 write.csv(INV, "Output/Data/INV.txt", row.names = FALSE)
 write.csv(SLMV, "Output/Data/SLMV.txt", row.names = FALSE)
 write.csv(ATG, "Output/Data/ATG.txt", row.names = FALSE)
-
