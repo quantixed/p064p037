88071466b7a9ed050b3de8cbd80bc8b9668e260e
Stephen Royle <s.j.royle@warwick.ac.uk>
2024-07-18 16:37:50 +0100

    Feat: enrichment pie charts

--- plots/ProteomicsComparison/Script/enrichment.R
+++ plots/ProteomicsComparison/Script/enrichment.R
@@ -1,4 +1,5 @@
 # enrichment analysis
+library(ggplot2)
 
 ## Functions ----
 
@@ -16,6 +17,40 @@ enrichment_test <- function(en1, total1,
   cat(paste(label2, "positive (%):", en2 / total2 * 100, "\n",
             label1, "positive (%):", en1 / total1 * 100, "\n"))
   print(chisq.test(df), similate.p.value = TRUE)
+}
+
+pie_maker <- function(x1, cols, x2 = NULL, label = "pie") {
+  df <- data.frame(
+    pvalues = x1,
+    pcolours = as.factor(seq_along(cols))
+  )
+  cs <- cumsum(x1)
+  df$ymin <- c(0, cs[seq_along(cs) - 1])
+  df$ymax <- cs
+  if (is.null(x2)) {
+    df2 <- df
+  } else {
+    cs <- cumsum(x2)
+    df2 <- data.frame(
+      pvalues = x2,
+      ymin = c(0, cs[seq_along(cs) - 1]),
+      ymax = cs
+    )
+  }
+  
+  ggplot() +
+    geom_rect(data = df, aes(fill = pcolours,
+                             ymin = ymin, ymax = ymax, xmin = 0, xmax = 3)) +
+    scale_fill_manual(values = cols) +
+    geom_rect(data = df2, aes(ymin = ymin, ymax = ymax,
+                              xmin = 0, xmax = 3),
+              alpha = 0, colour = "black") +
+    xlim(c(0, 3)) +
+    theme_minimal() +
+    theme(aspect.ratio = 1) +
+    coord_polar(theta = "y")
+  ggsave(paste0("Output/Plots/", label, ".pdf"),
+         width = 5, height = 3, units = "cm", dpi = 300)
 }
 
 ## Script ----
@@ -57,3 +92,25 @@ enrichment_test(
   label2 = "INV",
   title = "INVs and CCV dataset"
 )
+
+# ATG9 incidence in INVs
+pie_maker(x1 = c(143, 459),
+          cols = c("#44aa99", "#117733"), label = "pie_atg9_incidence_inv")
+# ATG9 incidence in all and in INVs
+pie_maker(x1 = c(392 - 143, 143, 459, 3264 - 459),
+          cols = c("#bbbbbb", "#44aa99", "#117733", "#dddddd"),
+          x2 = c(392, 3264), label = "pie_atg9_incidence_ip")
+# SLMV incidence in INVs
+pie_maker(x1 = c(33, 569),
+          cols = c("#44aa99", "#117733"), label = "pie_SLMV_incidence_inv")
+# SLMV incidence in all and in INVs
+pie_maker(x1 = c(67 - 33, 33, 569, 3589 - 569),
+          cols = c("#bbbbbb", "#44aa99", "#117733", "#dddddd"),
+          x2 = c(67, 3589), label = "pie_SLMV_incidence_ip")
+# CCV incidence in INVs
+pie_maker(x1 = c(9, 593),
+          cols = c("#44aa99", "#117733"), label = "pie_CCV_incidence_inv")
+# CCV incidence in all and in INVs
+pie_maker(x1 = c(61 - 9, 9, 593, 3595 - 593),
+          cols = c("#bbbbbb", "#44aa99", "#117733", "#dddddd"),
+          x2 = c(61, 3595), label = "pie_CCV_incidence_ip")
